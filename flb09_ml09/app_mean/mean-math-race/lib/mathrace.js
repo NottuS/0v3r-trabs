80ea1af023fe18b2b1775f17b7971f03c8742e82
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-12-01 00:23:21 -0200

    TG: V The Empire Strikes Back
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- flb09_ml09/app_mean/mean-math-race/lib/mathrace.js
+++ flb09_ml09/app_mean/mean-math-race/lib/mathrace.js
@@ -1,12 +1,129 @@
 module.exports = function(io) {
+	var lastOpSolution;
 	var sockets = io.sockets;
+	var lClient;
+	var clients = {};
+	var sessions = [];
+	var newOp = defineOp();
+	var scores = {}
+	var hall_of_fame = [];
+
 	sockets.on('connection', function (client) {
-		client.on('send-server', function (item) {
-			var msg = "<b>"+item.name+":</b> "+item.value+"<br>";
-			client.emit('send-client', "Server->client:"+msg+" Op:"+defineOp());
-			client.broadcast.emit('send-client', "Server->all:"+msg);
+		lClient = client;
+		client.on('send-server-result', function (item) {
+				var msg;
+				if (lastOpSolution == item.value) {
+					msg = "Acertou";
+					newOp = defineOp();
+					client.emit('send-client-result', 1);
+					client.broadcast.emit('send-client-result', 2);
+					client.emit('send-client', newOp);
+					client.broadcast.emit('send-client', newOp);
+					scores[item.name] = (scores[item.name] || 0) + 1;
+					broadcast ('scores', format_scores(scores)); //broacast scores
+				} else {
+					msg = "Errou";
+					client.emit('send-client-result', 0);
+				}	
+				client.broadcast.emit('send-client', newOp);
+				//newOp = defineOp();
+				// msg += "<b>"+item.name+":</b> "+item.value+"<br>";
+				// client.emit('send-client', "Server->client:"+msg+" Op:"+newOp);
+				// client.broadcast.emit('send-client', "Server->all:"+msg+" Op:"+newOp);
+		});
+
+		client.on('join', function (data) {
+			client.emit ('new_operation', newOp); //send challenge to new client
+			client.emit ('scores', format_scores(scores)); //send scores to new client
+			client.emit ('hall_of_fame', hall_of_fame); //send hall of fame
+			var newClient = true;
+			for (var i=0; i < sessions.length; i++) {
+				if (sessions[i] === client.id) {
+					newClient = false;
+				}
+			}
+			if (newClient) {
+				console.log("newClient: "+client.id);
+				sessions.push(client.id);
+				clients[client.id] = client;
+				// client.emit('send-client', newOp);
+				// client.emit ('scores', format_scores(scores)); //send scores to new client
+				// client.emit ('history', history); //send history
+				broadcast ('hall_of_fame', hall_of_fame); //send hall of fame
+		
+			}
 		});
 	});
+
+	var game_duration = 60 * 1000;
+	var game_started = new Date();
+
+	setInterval(function broadcastTime() {
+		var elapsed = new Date().getTime() - game_started.getTime();
+		var remaining = Math.floor((game_duration - elapsed) / 1000);
+		if (remaining<0){
+			var timestamp = game_started.getDate() + '/' + (game_started.getMonth() + 1) + '/' + game_started.getFullYear() + ' ' +  game_started.getHours() + ":" + (game_started.getMinutes() > 9 ? game_started.getMinutes() : '0' + game_started.getMinutes());
+			if (format_scores(scores).length)
+				hall_of_fame = UpdateHallOfFame(format_scores(scores), timestamp);
+			scores = {};
+			game_started = new Date(); //start game again!
+			newOp = defineOp();
+			broadcast("new-game",newOp);
+			broadcast ('scores', format_scores(scores)); //broadcast scores
+			broadcast ('hall_of_fame', hall_of_fame); //broadcast "hall of fame"
+		} else 
+			broadcast("time",remaining);
+	},1000);
+
+	function broadcast(type, msg) {
+		for (var i=0, l=sessions.length; i < l ; i++) {
+				clients[sessions[i]].emit(type, msg);
+		};
+	}
+
+	function UpdateHallOfFame(scores, timestamp){
+		var newPlayer = true;
+		// if(hall_of_fame.length)
+		   	for(var score_key in scores) { // Atualiza para o maior score realizado
+				for(var hof_key in hall_of_fame) {
+					if (hall_of_fame[hof_key].player == scores[score_key].player) {
+						if (hall_of_fame[hof_key].score < scores[score_key].score) {
+							hall_of_fame[hof_key].score = scores[score_key].score;
+						};
+						newPlayer = false;
+					};
+				}
+				if (newPlayer) {
+					scores[score_key].timestamp = timestamp;
+					hall_of_fame.push(scores[score_key]); //add all scores
+				};
+				newPlayer = true;
+			}
+
+		// for (var i = 0, l = scores.length; i < l ;  i++) {
+		// 	var score = scores[i];
+		// 	score.timestamp = timestamp;
+		// 	hall_of_fame.push(score); //add all scores
+		// };
+
+		//sort by score
+		hall_of_fame.sort(function(a,b) {
+      		return (b["score"] - a["score"]);
+		});
+		// util.sort(hall_of_fame, 'score', true);
+
+		//and slice array!
+		return hall_of_fame.slice(0,20);
+	}
+
+
+	function format_scores (scores){
+	   var arr = [];
+	   for(var key in scores){
+	      arr.push({player: key, score : scores[key]});
+	   }
+	   return arr;
+	}
 
 	function getRandomNumber(min, max) {
     	return Math.floor(Math.random() * (max - min + 1)) + min;	
@@ -16,6 +133,7 @@ module.exports = function(io) {
 		var number1 = getRandomNumber(0,20);
 		var number2 = getRandomNumber(0,20);
 		var operation_type = getRandomNumber(0,1) ? '+' : '-';
+		lastOpSolution = eval(number1 + operation_type + number2);
 		return number1 + operation_type + number2;
 	}
 }
--- flb09_ml09/app_mean/mean-math-race/public/javascripts/controllers.js
+++ flb09_ml09/app_mean/mean-math-race/public/javascripts/controllers.js
@@ -6,10 +6,76 @@ angular.module('sonApp.controllers', ['sonApp.services'])
 			 	$scope.valid = false;
 			 	var socket = io.connect('http://localhost:3000');
 
+				socket.on('connect',function() {
+					var connSocketio = document.getElementById('socketio');
+					connSocketio.innerHTML = '<span class="label label-success">connected!</label>'; 
+					socket.emit('join', "{ id: user_id }");
+				});
+			 	
+			 	socket.on('send-client-result', function (resultValue) {
+			 		var result = document.getElementById('result');
+					if (resultValue == 1) {
+						result.innerHTML = "Você acertou!!!";
+					} else if (resultValue == 2) {
+						result.innerHTML = "Alguem acertou antes de você!!!";
+					} else {
+						result.innerHTML = "Você errou!!!";
+					}
+
+				});
+
 			 	socket.on('send-client', function (msg) {
-					var chat = document.getElementById('op');
-					chat.innerHTML += msg;
-					$scope.item.value = "msg do sv:"+msg;
+					var op = document.getElementById('op');
+					op.innerHTML = msg;
+					//$scope.op = msg;
+				});
+
+			 	socket.on('time', function (msg) {
+			 		// alert("Foi"+time);
+					var mathTime = document.getElementById('mrtime');
+					mathTime.innerHTML = msg;
+					$scope.mTime = "time: "+msg;
+					//$scope.op = msg;
+				});
+
+				socket.on('new-game', function (msg) {
+					// print_msg ('new game, new score, hurry up!!', 'new', 2000);
+					var op = document.getElementById('op');
+					op.innerHTML = msg;
+				});
+
+				socket.on('new_operation',function(operation) {
+					var op = document.getElementById('op');
+					op.innerHTML = operation;
+				});
+				
+				socket.on('scores',function(scores) {
+					// race.scores(sort(scores,'score', true));
+					var score = document.getElementById('score');
+					score.innerHTML= "";
+					if (scores.length){ //effect
+					   for(var key in scores){
+						score.innerHTML += "<br>" + scores[key].player + " : " + scores[key].score;
+					  }
+					  // $('.scores').addClass('selected');
+					  // setTimeout(function (){
+					  //     $('.scores').removeClass('selected')
+					  // }, 200);
+					}
+				});
+
+      			socket.on('hall_of_fame',function(hof) {
+      				// TODO: Falta ordenar pelo score
+      				var hall_of_fame = document.getElementById('hall_of_fame');
+					hall_of_fame.innerHTML= "/";
+      				for(var key in hof){
+						hall_of_fame.innerHTML += "<br>" + hof[key].player + " : " + hof[key].score + "    " + hof[key].timestamp;
+				   }
+
+				});
+
+				socket.on('disconnect',function() {
+					// $('#socketio').html('<span class="label label-important">disconnected</label>'); 
 				});
 
 				$scope.load = function() {
@@ -61,9 +127,11 @@ angular.module('sonApp.controllers', ['sonApp.services'])
 					 }					
 				}
 
-				$scope.sendResult = function(item) {
-					item.value += "Foi"; 
-					socket.emit('send-server', item);
+				$scope.sendResult = function(item) { 
+					if (item.value) {
+						socket.emit('send-server-result', item);
+					};
+					// $scope.op += "\nEnviou: "+item.value;
 				}
 
 				$scope.race = function(item) {
