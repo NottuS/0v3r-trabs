b3e2046ce3911057f8e8f64f17d66310e21f58fa
nottus <edileuton@gmail.com>
2016-01-17 16:28:57 -0200

    ffff

--- trab3/SimpleGPUCall.cpp
+++ trab3/SimpleGPUCall.cpp
@@ -11,27 +11,27 @@ int main(int argc, char *argv[]) {
 	m = atoi(argv[2]);
 	cycles = atoi(argv[3]);
 	printBoard = atoi(argv[4]);
-	/*clFactory::setDeviceType(RUN_ON_GPU);
+	clFactory::setDeviceType(RUN_ON_GPU);
 	clFactory::startup();
 	ClGol *clGol =  new ClGol();
 
 
-	clGol->setCLQueue(clFactory::getQueue());
-    clGol->initializeCL();
-    clGol->runGolkernels(n, m, cycles, printBoard);
-	clFactory::shutdown();
-
-	delete clGol;*/
-
-	clFactory::setDeviceType(RUN_ON_CPU);
-	clFactory::startup();
-	ClGol *clGol =  new ClGol();
 	clGol->setCLQueue(clFactory::getQueue());
     clGol->initializeCL();
     clGol->runGolkernels(n, m, cycles, printBoard);
 	clFactory::shutdown();
 
 	delete clGol;
+
+	/*clFactory::setDeviceType(RUN_ON_CPU);
+	clFactory::startup();
+	clGol =  new ClGol();
+	clGol->setCLQueue(clFactory::getQueue());
+    clGol->initializeCL();
+    clGol->runGolkernels(n, m, cycles, printBoard);
+	clFactory::shutdown();
+
+	delete clGol;*/
 
 	return 0;
 }
